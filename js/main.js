50367de53e1e61ba14940a42516678f90caa2d44
Reece Walter <reece@reecewalter.com>
2021-03-07 19:57:24 -0500

    named switch function to dry up code"
    test
    c

--- js/main.js
+++ js/main.js
@@ -15,62 +15,37 @@ function winConditions() {
     let win7 = row1[0] + row2[1] + row3[2]
     let win8 = row1[2] + row2[1] + row3[0]
 
-    switch (3) {
-        case win1:
-            console.log('1')
-            break;
-        case win2:
-            console.log('2')
-            break;
-        case win3:
-            console.log('3')
-            break;
-        case win4:
-            console.log('4')
-            break;
-        case win5:
-            console.log('5')
-            break;
-        case win6:
-            console.log('6')
-            break;
-        case win7:
-            console.log('7')
-            break;
-        case win8:
-            console.log('8')
-            break;
-        default:
-            console.log("Other Player's Turn")
+    function xOrO {
+        switch (X) {
+            case win1:
+                console.log('1')
+                break;
+            case win2:
+                console.log('2')
+                break;
+            case win3:
+                console.log('3')
+                break;
+            case win4:
+                console.log('4')
+                break;
+            case win5:
+                console.log('5')
+                break;
+            case win6:
+                console.log('6')
+                break;
+            case win7:
+                console.log('7')
+                break;
+            case win8:
+                console.log('8')
+                break;
+            default:
+                console.log("Other Player's Turn")
+        }
     }
-
-    switch (-3) {
-        case win1:
-            console.log('1')
-            break;
-        case win2:
-            console.log('2')
-            break;
-        case win3:
-            console.log('3')
-            break;
-        case win4:
-            console.log('4')
-            break;
-        case win5:
-            console.log('5')
-            break;
-        case win6:
-            console.log('6')
-            break;
-        case win7:
-            console.log('7')
-            break;
-        case win8:
-            console.log('8')
-            break;
-        default:
-            console.log("Other Player's Turn")
-
-    }
+    
+    xOrO(3);
+    xOrO(-3);
 }

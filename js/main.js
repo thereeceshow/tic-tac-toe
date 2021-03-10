e6db98ddc5b4903e927a46d5b9dfd11ab9e9677a
Reece Walter <reece@reecewalter.com>
2021-03-10 12:00:12 -0500

    works, no reset

--- js/main.js
+++ js/main.js
@@ -3,32 +3,108 @@ class Board {
         this.boardArray = []
         this.tileClick = this.tileClick.bind(this);
         this.turnCount = 0
+        this.xIsNeg = -3
     }
     render() {
         let mainDiv = document.getElementById('mainDiv')
+        let title = document.createElement('h1');
+        title.setAttribute('class', 'mx-5 justify-content-center text-center');
+        title.innerText = 'Galactic Tic-Tac-Toe';
+        mainDiv.appendChild(title);
         let board = document.createElement('div');
         board.setAttribute('class', 'row mx-5 justify-content-center');
         mainDiv.appendChild(board);
-        console.log(board);
-        console.log(this);
 
         for (let i = 0; i < 9; i++) {
             const tileObject = new Tile(i);
             let tileHtml = document.createElement('div');
             tileHtml.setAttribute('class', 'col-4 p-5 border square text-center');
             tileHtml.setAttribute('id', i);
-            tileHtml.addEventListener('click', e=> this.tileClick(i)); // <---- access local members in an object you need to use 'this'
+            tileHtml.addEventListener('click', e => this.tileClick(i)); // <---- access local members in an object you need to use 'this'
             board.appendChild(tileHtml);
             tileObject.html = tileHtml
             this.boardArray.push(tileObject)
         }
+
     }
-    tileClick (index) {
+    tileClick(index) {
         //console.log(index);
-        this.turnCount++
-        this.boardArray[index].redTile(this.turnCount);
+        if (this.boardArray[index].locked === false) {
+            this.turnCount++
+            this.boardArray[index].usedTile(this.turnCount);
+            this.boardArray[index].locked = true;
+            if (this.turnCount >= 5) {
+                this.winCondition()
+            }
+        }
+    }
+    winCondition() {
+        if (this.turnCount % 2 === 0) {
+            this.xIsNeg = 3
+        } else {
+            this.xIsNeg = -3
+        }
+        let mainDiv = document.getElementById('mainDiv')
 
+        if (this.boardArray[0].displayState + this.boardArray[1].displayState + this.boardArray[2].displayState == this.xIsNeg) {
+            console.log(this.xIsNeg === 3 ? "The Empire is the Winner" : "The Rebels have Won!!!")
 
+            this.boardArray[0].html.classList.add('bg-danger')
+            this.boardArray[1].html.classList.add('bg-danger')
+            this.boardArray[2].html.classList.add('bg-danger')
+            mainDiv.classList.add('gameOver')
+            
+        } else if (this.boardArray[3].displayState + this.boardArray[4].displayState + this.boardArray[5].displayState == this.xIsNeg) {
+            console.log(this.xIsNeg === 3 ? "The Empire is the Winner" : "The Rebels have Won!!!")
+            this.boardArray[3].html.classList.add('bg-danger')
+            this.boardArray[4].html.classList.add('bg-danger')
+            this.boardArray[5].html.classList.add('bg-danger')
+            mainDiv.classList.add('gameOver')
+            
+        } else if (this.boardArray[6].displayState + this.boardArray[7].displayState + this.boardArray[8].displayState == this.xIsNeg) {
+            console.log(this.xIsNeg === 3 ? "The Empire is the Winner" : "The Rebels have Won!!!")
+            this.boardArray[6].html.classList.add('bg-danger')
+            this.boardArray[7].html.classList.add('bg-danger')
+            this.boardArray[8].html.classList.add('bg-danger')
+            mainDiv.classList.add('gameOver')
+            
+        } else if (this.boardArray[0].displayState + this.boardArray[3].displayState + this.boardArray[6].displayState == this.xIsNeg) {
+            console.log(this.xIsNeg === 3 ? "The Empire is the Winner" : "The Rebels have Won!!!")
+            this.boardArray[0].html.classList.add('bg-danger')
+            this.boardArray[3].html.classList.add('bg-danger')
+            this.boardArray[6].html.classList.add('bg-danger')
+            mainDiv.classList.add('gameOver')
+            
+        } else if (this.boardArray[1].displayState + this.boardArray[4].displayState + this.boardArray[7].displayState == this.xIsNeg) {
+            console.log(this.xIsNeg === 3 ? "The Empire is the Winner" : "The Rebels have Won!!!")
+            this.boardArray[1].html.classList.add('bg-danger')
+            this.boardArray[4].html.classList.add('bg-danger')
+            this.boardArray[7].html.classList.add('bg-danger')
+            mainDiv.classList.add('gameOver')
+            
+        } else if (this.boardArray[2].displayState + this.boardArray[5].displayState + this.boardArray[8].displayState == this.xIsNeg) {
+            console.log(this.xIsNeg === 3 ? "The Empire is the Winner" : "The Rebels have Won!!!")
+            this.boardArray[2].html.classList.add('bg-danger')
+            this.boardArray[5].html.classList.add('bg-danger')
+            this.boardArray[8].html.classList.add('bg-danger')
+            mainDiv.classList.add('gameOver')
+            
+        } else if (this.boardArray[0].displayState + this.boardArray[4].displayState + this.boardArray[8].displayState == this.xIsNeg) {
+            console.log(this.xIsNeg === 3 ? "The Empire is the Winner" : "The Rebels have Won!!!")
+            this.boardArray[0].html.classList.add('bg-danger')
+            this.boardArray[4].html.classList.add('bg-danger')
+            this.boardArray[8].html.classList.add('bg-danger')
+            mainDiv.classList.add('gameOver')
+            
+        } else if (this.boardArray[6].displayState + this.boardArray[4].displayState + this.boardArray[2].displayState == this.xIsNeg) {
+            console.log(this.xIsNeg === 3 ? "The Empire is the Winner" : "The Rebels have Won!!!");
+            this.boardArray[6].html.classList.add('bg-danger')
+            this.boardArray[4].html.classList.add('bg-danger')
+            this.boardArray[2].html.classList.add('bg-danger')
+            mainDiv.classList.add('gameOver')
+        } else {
+            console.log("Other Player's Turn")
+        }
     }
 }
 
@@ -37,21 +113,24 @@ class Tile {
         this.html = ''
         this.displayState = ''
         this.id = id
+        this.locked = false
     }
     render() {
         // display blank, X, or O
     }
-    redTile (turnCount) {
+    usedTile(turnCount) {
         console.log(this.id);
         console.log(turnCount)
         if (turnCount % 2 === 0) {
-            this.html.classList.add('bg-success')
-            this.html.innerHTML = '<i class="fab fa-sith fa-3x"></i>';
+            //this.html.classList.add('bg-success')
+            this.html.innerHTML = '<i class="fab fa-empire fa-3x"></i>';
+            this.displayState = 1;
         } else {
-            this.html.classList.add('bg-danger')
+            //this.html.classList.add('bg-danger')
             this.html.innerHTML = '<i class="fab fa-rebel fa-3x"></i>';
+            this.displayState = -1;
         }
-        
+
 
 
     }
@@ -60,7 +139,6 @@ class Tile {
 function init() {
     const board1 = new Board()
     board1.render()
-    console.log(board1);
-    
+
 
 }
--- js/main2.js
+++ js/main2.js
@@ -116,52 +116,53 @@ function init (){
 // Board is represented by these arrays
 // 0 is empty, X is 1, O is -1.
 
-// let row1 = [0, 0, 0]
-// let row2 = [0, 0, 0]
-// let row3 = [0, 0, 0]
+let row1 = [0, 0, 0]
+let row2 = [0, 0, 0]
+let row3 = [0, 0, 0]
 
-// function winConditions() {
-//     let win1 = row1[0] + row1[1] + row1[2]
-//     let win2 = row2[0] + row2[1] + row2[2]
-//     let win3 = row3[0] + row3[1] + row3[2]
-//     let win4 = row1[0] + row2[0] + row3[0]
-//     let win5 = row1[1] + row2[1] + row3[1]
-//     let win6 = row1[2] + row2[2] + row3[2]
-//     let win7 = row1[0] + row2[1] + row3[2]
-//     let win8 = row1[2] + row2[1] + row3[0]
+function winConditions() {
 
-//     function xOrO {
-//         switch (X) {
-//             case win1:
-//                 console.log('1')
-//                 break;
-//             case win2:
-//                 console.log('2')
-//                 break;
-//             case win3:
-//                 console.log('3')
-//                 break;
-//             case win4:
-//                 console.log('4')
-//                 break;
-//             case win5:
-//                 console.log('5')
-//                 break;
-//             case win6:
-//                 console.log('6')
-//                 break;
-//             case win7:
-//                 console.log('7')
-//                 break;
-//             case win8:
-//                 console.log('8')
-//                 break;
-//             default:
-//                 console.log("Other Player's Turn")  // `Now its ${otherPlayer}'s turn.`
-//         }
-//     }
-//     // if X turn
-//     xOrO(3);
-//     // if O turn
-//     xOrO(-3);
-// }
+    let win1 = this.boardArray[0] + this.boardArray[1] + this.boardArray[2]
+    let win2 = this.boardArray[3] + this.boardArray[4] + this.boardArray[5]
+    let win3 = this.boardArray[6] + this.boardArray[7] + this.boardArray[8]
+    let win4 = this.boardArray[1] + this.boardArray[4] + this.boardArray[7]
+    let win5 = this.boardArray[2] + this.boardArray[5] + this.boardArray[8]
+    let win6 = this.boardArray[3] + this.boardArray[6] + this.boardArray[9]
+    let win7 = this.boardArray[1] + this.boardArray[5] + this.boardArray[9]
+    let win8 = this.boardArray[7] + this.boardArray[5] + this.boardArray[3]
+
+    function xOrO {
+        switch (X) {
+            case win1:
+                console.log('1')
+                break;
+            case win2:
+                console.log('2')
+                break;
+            case win3:
+                console.log('3')
+                break;
+            case win4:
+                console.log('4')
+                break;
+            case win5:
+                console.log('5')
+                break;
+            case win6:
+                console.log('6')
+                break;
+            case win7:
+                console.log('7')
+                break;
+            case win8:
+                console.log('8')
+                break;
+            default:
+                console.log("Other Player's Turn")  // `Now its ${otherPlayer}'s turn.`
+        }
+    }
+    // if X turn
+    xOrO(3);
+    // if O turn
+    xOrO(-3);
+}
